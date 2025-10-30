68c33b33f39ef557ad9091f6991f623ae1277eed
Ryan Qu <130172906+Ryanqu27@users.noreply.github.com>
2025-10-30 00:48:28 -0400

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String scramble = ScrambleGenerator.generateScramble(20);
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            CubeUI cubeUI = new CubeUI(scramble);
-            cubeUI.setVisible(true);
-        });
-    }
-}
